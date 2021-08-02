595ce10f631b526cb9eb5f81a6fefcf7fdea507c
ThamesDev <freddie@jemison.co.uk>
2021-08-02 23:03:54 +0100

    Add member list for bot

--- not-a-robot.py
+++ not-a-robot.py
@@ -22,4 +22,8 @@ async def on_ready():
     print(f'{client.user} has connected to the following server:\n'
           f'{guild.name} (id: {guild.id})')
 
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+
 client.run(TOKEN)
